ca523b55a80f2a4932b51fb0c744f272439ffb46
Rebel Designs <81110168+Naz94@users.noreply.github.com>
2026-03-23 14:21:49 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+module.exports = nextConfig
+```
+
+Make sure your repo structure looks exactly like this:
+```
+rebel-shap/
+├── pages/
+│   ├── index.js
+│   └── _app.js
+├── next.config.js
+├── package.json
+└── README.md
