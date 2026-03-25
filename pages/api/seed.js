82793da36348ad950bf9367b1a071b9e2facc60c
Rebel Designs <81110168+Naz94@users.noreply.github.com>
2026-03-25 09:53:49 +0200

    Update seed.js

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -10,7 +10,6 @@
 // ============================================================
 
 import { createTenant, getTenant } from "../../lib/tenants.js";
-import { requireAuth }             from "../../lib/auth.js";
 
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
